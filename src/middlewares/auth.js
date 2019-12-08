b80a4415e5532fd06e09c6875ef2f6954db0a77b
raidou12 <icarodaviduarte@gmail.com>
2019-12-08 16:20:09 -0300

    fix bug addMessage, add chat userType

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -24,7 +24,7 @@ module.exports = (req, res, next) => {
             return res.status(401).send({ error: 'Token invalid!' });
             
         req.userId = decoded.id;
-        
+
         if(await authenticate(res, decoded.id)) return next();
         else return res.status(401).send({error: 'Token invalido.'});
     });
